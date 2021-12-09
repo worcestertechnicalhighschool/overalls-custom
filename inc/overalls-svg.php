75e24409efd21f2666d74759c72c04930d26ec5c
jeffhow <jeff@jeff.how>
2021-12-09 12:19:53 -0500

    fixed logo

--- inc/overalls-svg.php
+++ inc/overalls-svg.php
@@ -1,4 +1,4 @@
-<?xml version="1.0" encoding="utf-8"?>
+<?php echo '<?xml version="1.0" encoding="utf-8"?>
 <!-- Generator: Adobe Illustrator 25.2.3, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
 <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 	 viewBox="0 0 459.79 200.02" style="enable-background:new 0 0 459.79 200.02;" xml:space="preserve">
@@ -573,4 +573,5 @@
 		C57.64,102.35,58.64,101.98,59.33,103.19z"/>
 </g>
 </g>
-</svg>
+</svg>'
+?>
